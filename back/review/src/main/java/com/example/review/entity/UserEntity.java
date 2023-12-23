dcff70fbf11783c7e31b790efd15934edeb8a336
AntonenkoTymofii <antonenkotimofii@gmail.com>
2023-12-23 17:16:07 +0200

    create entity

---
+++ back/review/src/main/java/com/example/review/entity/UserEntity.java
@@ -0,0 +1,65 @@
+package com.example.review.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "User")
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+    private String hash;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String phone;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getHash() {
+        return hash;
+    }
+
+    public void setHash(String hash) {
+        this.hash = hash;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
