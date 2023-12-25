894bb949aa449d26691979989f11d8716c2c9e28
AntonenkoTymofii <antonenkotimofii@gmail.com>
2023-12-26 01:03:06 +0200

    connect classes

--- back/review/src/main/java/com/example/review/controller/UserController.java
+++ back/review/src/main/java/com/example/review/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.example.review.controller;
 
+import com.example.review.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
 
     @GetMapping
     public ResponseEntity getUser(){
--- back/review/src/main/java/com/example/review/service/UserService.java
+++ back/review/src/main/java/com/example/review/service/UserService.java
@@ -1,8 +1,12 @@
 package com.example.review.service;
 
+import com.example.review.repository.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+    @Autowired
+    private UserRepo userRepo;
 
 }
