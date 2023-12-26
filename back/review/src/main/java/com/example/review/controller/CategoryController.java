ea9e7e6672212ea09a7fc2c306679e3097fcaebb
AntonenkoTymofii <antonenkotimofii@gmail.com>
2023-12-26 02:01:43 +0200

    start realise Category CRUD

---
+++ back/review/src/main/java/com/example/review/controller/CategoryController.java
@@ -0,0 +1,22 @@
+package com.example.review.controller;
+
+import com.example.review.exception.UserNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class CategoryController {
+    @GetMapping
+    public ResponseEntity getCategory(){
+        try {
+            return ResponseEntity.ok("Сервер працює");
+        } catch (Exception exception){
+            return ResponseEntity.badRequest().body("Відбулась помилка");
+        }
+    }
+    //TODO: Написати реалізацію CRUD для Category
+}
---
+++ back/review/src/main/java/com/example/review/entity/CategoryEntity.java
@@ -0,0 +1,30 @@
+package com.example.review.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "\"Category\"")
+public class CategoryEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
