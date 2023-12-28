8caf480f626a91ab6a640217f03caf8735a0210c
AntonenkoTymofii <antonenkotimofii@gmail.com>
2023-12-28 18:49:06 +0200

    add get request to controller

--- back/review/src/main/java/com/example/review/controller/CategoryController.java
+++ back/review/src/main/java/com/example/review/controller/CategoryController.java
@@ -2,6 +2,7 @@ package com.example.review.controller;
 
 import com.example.review.entity.CategoryEntity;
 import com.example.review.exception.categoryExceptions.CategoryNameAlreadyExistException;
+import com.example.review.exception.categoryExceptions.CategoryNotFoundException;
 import com.example.review.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,7 +14,7 @@ public class CategoryController {
 
     @Autowired
     private CategoryService categoryService;
-    @PostMapping
+    @PostMapping("/create")
     public ResponseEntity createCategory(@RequestBody
                                              CategoryEntity categoryEntity){
         try {
@@ -25,13 +26,29 @@ public class CategoryController {
             return ResponseEntity.badRequest().body("Відбулась помилка");
         }
     }
-    @GetMapping
-    public ResponseEntity getCategoryByName(){
+
+    @GetMapping("/search")
+    public ResponseEntity getCategoryByName(@RequestParam String name){
         try {
-            return ResponseEntity.ok("Сервер працює");
+            return ResponseEntity.ok(categoryService.getCategoryByName(name));
+        } catch (CategoryNotFoundException exception){
+            return ResponseEntity.badRequest().body(exception.getMessage());
         } catch (Exception exception){
             return ResponseEntity.badRequest().body("Відбулась помилка");
         }
     }
+
+    @GetMapping("/search")
+    public ResponseEntity getCategoryById(@RequestParam Long id){
+        try {
+            return ResponseEntity.ok(categoryService.getCategoryById(id));
+        } catch (CategoryNotFoundException exception){
+            return ResponseEntity.badRequest().body(exception.getMessage());
+        } catch (Exception exception){
+            return ResponseEntity.badRequest().body("Відбулась помилка");
+        }
+    }
+
+
     //TODO: Написати реалізацію CRUD для Category
 }
