ad71b3da8f9ffd1c4e08d4b0d580bfd8323d7405
AntonenkoTymofii <antonenkotimofii@gmail.com>
2023-12-30 15:27:24 +0200

    create shop controller and checked it efficiency

--- back/review/src/main/java/com/example/review/controller/ShopController.java
+++ back/review/src/main/java/com/example/review/controller/ShopController.java
@@ -1,5 +1,7 @@
 package com.example.review.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/shop")
 public class ShopController {
 
+    @GetMapping
+    public ResponseEntity getShop(){
+        try {
+            return ResponseEntity.ok("Сервіс працює");
+        } catch (Exception exception){
+            return ResponseEntity.badRequest().body("Відбулась помилка");
+        }
+    }
 }
