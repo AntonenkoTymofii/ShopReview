3bef31d4b3182f93e88165d54dd3a1234c12c419
AntonenkoTymofii <antonenkotimofii@gmail.com>
2024-01-03 18:54:02 +0200

    create product exception

---
+++ back/review/src/main/java/com/example/review/exception/productExceptions/ProductNotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.review.exception.productExceptions;
+
+public class ProductNotFoundException extends Exception{
+    public ProductNotFoundException(String message) {
+        super(message);
+    }
+}
