a3a315e0855f192fc7b0497bcca82f010a8a370f
AntonenkoTymofii <antonenkotimofii@gmail.com>
2024-01-03 18:44:13 +0200

    create post request

--- back/review/src/main/java/com/example/review/controller/ProductController.java
+++ back/review/src/main/java/com/example/review/controller/ProductController.java
@@ -1,13 +1,12 @@
 package com.example.review.controller;
 
+import com.example.review.entity.ProductEntity;
 import com.example.review.exception.shopException.ShopNameAlreadyExistException;
 import com.example.review.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/product")
@@ -15,6 +14,16 @@ public class ProductController {
 
     @Autowired
     private ProductService productService;
+
+    @PostMapping("/add")
+    public ResponseEntity createProduct(@RequestBody ProductEntity productEntity){
+        try {
+            productService.createProduct(productEntity);
+            return ResponseEntity.ok("Продукт було успішно створено");
+        } catch (Exception exception){
+            return ResponseEntity.badRequest().body("Відбулась помилка");
+        }
+    }
 
     @GetMapping
     public ResponseEntity getProductById(Long id){
--- back/review/src/main/java/com/example/review/service/ProductService.java
+++ back/review/src/main/java/com/example/review/service/ProductService.java
@@ -1,5 +1,8 @@
 package com.example.review.service;
 
+import com.example.review.entity.CategoryEntity;
+import com.example.review.entity.ProductEntity;
+import com.example.review.exception.categoryExceptions.CategoryNameAlreadyExistException;
 import com.example.review.repository.ProductRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,4 +13,7 @@ public class ProductService {
     @Autowired
     private ProductRepo productRepo;
 
+    public void createProduct(ProductEntity productEntity) {
+        productRepo.save(productEntity);
+    }
 }
