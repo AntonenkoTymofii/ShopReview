106439492bb7b7a488bc128b11bc850fe07956cd
AntonenkoTymofii <antonenkotimofii@gmail.com>
2024-01-03 15:48:25 +0200

    create product repo

---
+++ back/review/src/main/java/com/example/review/repository/ProductRepo.java
@@ -0,0 +1,10 @@
+package com.example.review.repository;
+
+import com.example.review.entity.ProductEntity;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface ProductRepo extends CrudRepository<ProductEntity, Long> {
+    List<ProductEntity> findByName(String name);
+}
