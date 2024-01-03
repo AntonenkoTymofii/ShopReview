dd6f4592f3a947f38a133c4f5ef07c997a8fa09d
AntonenkoTymofii <antonenkotimofii@gmail.com>
2024-01-03 18:53:47 +0200

    create toModel method

--- back/review/src/main/java/com/example/review/model/Product.java
+++ back/review/src/main/java/com/example/review/model/Product.java
@@ -1,7 +1,44 @@
 package com.example.review.model;
 
+import com.example.review.entity.ProductEntity;
+
 public class Product {
     private Long id;
     private String name;
     private Float price;
+
+    public static Product toModel(ProductEntity productEntity){
+        Product model = new Product();
+        model.setId(productEntity.getId());
+        model.setName(productEntity.getName());
+        model.setPrice(productEntity.getPrice());
+        return model;
+    }
+
+    public Product() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
 }
