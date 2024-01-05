92a1feb265344f2f6beacb519d2c7bfc22f63741
AntonenkoTymofii <antonenkotimofii@gmail.com>
2024-01-06 01:51:01 +0200

    edit migration

--- back/review/src/main/resources/db/migration/V3__CREATE_PURCHASE_PRODUCT_TABLE.sql
+++ back/review/src/main/resources/db/migration/V3__CREATE_PURCHASE_PRODUCT_TABLE.sql
@@ -1,5 +1,3 @@
-ALTER TABLE purchase DROP COLUMN id_product;
-
 create table if not exists "ShopReviewDB"."public"."purchaseProduct"(
         id BIGINT PRIMARY KEY NOT NULL,
         id_purchase BIGINT REFERENCES "purchase"(id) NOT NULL,
