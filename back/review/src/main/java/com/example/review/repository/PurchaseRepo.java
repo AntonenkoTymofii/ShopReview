a0371f832f86e32734503ffef5c0c54312a274c1
AntonenkoTymofii <antonenkotimofii@gmail.com>
2024-01-06 03:15:18 +0200

    checked works

--- back/review/src/main/java/com/example/review/repository/PurchaseRepo.java
+++ back/review/src/main/java/com/example/review/repository/PurchaseRepo.java
@@ -2,10 +2,13 @@ package com.example.review.repository;
 
 import com.example.review.entity.PurchaseEntity;
 import com.example.review.entity.ShopEntity;
+import jakarta.persistence.Column;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
-public interface PurchaseRepo extends CrudRepository<PurchaseRepo, Long> {
-    List<PurchaseEntity> findByIdShop(Long id_shop);
+public interface PurchaseRepo extends CrudRepository<PurchaseEntity, Long> {
+    List<PurchaseEntity> findByShopId(Long ShopId);
 }
