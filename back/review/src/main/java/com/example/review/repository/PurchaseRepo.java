79f9da9c00784c4aa5917477f29096fc302498d3
AntonenkoTymofii <antonenkotimofii@gmail.com>
2024-01-06 03:16:47 +0200

    delete not needs imports

--- back/review/src/main/java/com/example/review/repository/PurchaseRepo.java
+++ back/review/src/main/java/com/example/review/repository/PurchaseRepo.java
@@ -1,11 +1,8 @@
 package com.example.review.repository;
 
 import com.example.review.entity.PurchaseEntity;
-import com.example.review.entity.ShopEntity;
-import jakarta.persistence.Column;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 import java.util.List;
 
